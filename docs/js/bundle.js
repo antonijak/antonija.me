61929983ba3d8233ded438a4b7e1f57337f56970
Antonija K <antonija.kasum@gmail.com>
2019-05-22 14:46:01 +0300

    fix/styles fixed accourding to google warning

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1,51 +1,73 @@
-const headerText = document.querySelector(".main-header__text__profession");
+//set header text
 
-headerText.textContent="<Frontend developer />"
+const frontendDeveloper = document.querySelector(
+	'.main-header__text__profession'
+);
 
-const email = document.querySelector(".contact__text__email");
+frontendDeveloper.textContent = '<Frontend developer />';
 
-email.addEventListener("mouseover", showCopyEmail);
+// Copy email to clipboard
 
-let bla;
-let bla2;
+const email = document.querySelector('.contact__text__email');
 
-function showCopyEmail () {
-    window.screen.width < 600 ? email.style.fontSize = '1.1rem': email.style.fontSize = '1.4rem';
-    email.textContent = "Copy email";
-    email.addEventListener("click", copyToClipboard);
-    email.addEventListener("mouseout", hideCopyEmail);
+function copyToClipboard() {
+	const contactText = document.querySelector('.contact__text');
+	const input = document.createElement('input');
+
+	contactText.appendChild(input);
+	input.setAttribute('class', 'contact__text__copy');
+	input.value = 'hello@antonijakasum.com';
+
+	input.select();
+	document.execCommand('copy');
+	email.setAttribute('class', 'copied');
+	contactText.removeChild(input);
+	email.addEventListener('mouseout', changeBack);
 }
 
-function hideCopyEmail () {
-
-    clearInterval(bla);
-    clearInterval(bla2);
-    email.textContent = "hello@antonijakasum.com";
-    email.style.background = "#FFECEC";
-    email.style.color = "#F55252";
+function changeBack() {
+	email.setAttribute('class', 'contact__text__email');
 }
 
-function copyToClipboard () {
-    const text = document.querySelector(".contact__text__copy")
-    text.select();
-    document.execCommand('copy');
-    email.textContent = "Email copied";
-    email.style.background = "#D8F4E7";
-    email.style.color = "#45C588";
-    email.style.fontSize = '1.4rem';
+email.addEventListener('click', copyToClipboard);
 
-    // email.style.transform = "scale(1.1, 1.1)"
-    // setTimeout(()=>{email.style.transform = "scale(1, 1)"}, 500);
-    // email.style.transform = "scale(1.1, 1.1)"
-    
-    
-    email.removeEventListener("click", copyToClipboard);
-       bla =  setInterval(function() {
-        email.style.fontSize = (email.style.fontSize === '1.4rem' ? '1.2rem' : '1.4rem');
-    }, 100);
-       bla2 = setInterval(function() {clearInterval(bla)
-        window.screen.width < 600 ? email.style.fontSize = '1.1rem': email.style.fontSize = '1.4rem';
-    }, 500);
-    
+// Show/hide video modal
+const modal = document.querySelector('.modal');
+const video = document.querySelector('.modal__video');
+frontendDeveloper.addEventListener('click', showModal);
+
+function showModal() {
+	modal.style.display = 'flex';
+	modal.addEventListener('click', hideModal);
 }
 
+function hideModal() {
+	modal.style.display = 'none';
+	player.stopVideo();
+}
+
+//create video (YT API)
+const tag = document.createElement('script');
+
+tag.src = 'https://www.youtube.com/iframe_api';
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+let player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		height: '390',
+		width: '640',
+		videoId: 'vZfr0G4BdD0',
+		events: {
+			onReady: onPlayerReady
+		}
+	});
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+	event.target.playVideo();
+}
