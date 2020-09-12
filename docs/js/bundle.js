83ad0f2b2a06d23a3292696957da8a3a0779ffe4
Antonija K <antonija.kasum@gmail.com>
2020-09-12 23:45:57 +0300

    remove video

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -32,42 +32,42 @@ function changeBack() {
 email.addEventListener('click', copyToClipboard);
 
 // Show/hide video modal
-const modal = document.querySelector('.modal');
-const video = document.querySelector('.modal__video');
-frontendDeveloper.addEventListener('click', showModal);
+// const modal = document.querySelector('.modal');
+// const video = document.querySelector('.modal__video');
+// frontendDeveloper.addEventListener('click', showModal);
 
-function showModal() {
-	modal.style.display = 'flex';
-	modal.addEventListener('click', hideModal);
-}
+// function showModal() {
+// 	modal.style.display = 'flex';
+// 	modal.addEventListener('click', hideModal);
+// }
 
-function hideModal() {
-	modal.style.display = 'none';
-	player.stopVideo();
-}
+// function hideModal() {
+// 	modal.style.display = 'none';
+// 	player.stopVideo();
+// }
 
 //create video (YT API)
-const tag = document.createElement('script');
+// const tag = document.createElement('script');
 
-tag.src = 'https://www.youtube.com/iframe_api';
-const firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// tag.src = 'https://www.youtube.com/iframe_api';
+// const firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-let player;
-function onYouTubeIframeAPIReady() {
-	player = new YT.Player('player', {
-		height: '390',
-		width: '640',
-		videoId: 'vZfr0G4BdD0',
-		events: {
-			onReady: onPlayerReady
-		}
-	});
-}
+// // 3. This function creates an <iframe> (and YouTube player)
+// //    after the API code downloads.
+// let player;
+// function onYouTubeIframeAPIReady() {
+// 	player = new YT.Player('player', {
+// 		height: '390',
+// 		width: '640',
+// 		videoId: 'vZfr0G4BdD0',
+// 		events: {
+// 			onReady: onPlayerReady
+// 		}
+// 	});
+// }
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-	event.target.playVideo();
-}
+// // 4. The API will call this function when the video player is ready.
+// function onPlayerReady(event) {
+// 	event.target.playVideo();
+// }
